72211f6ed44774dac24a48206024b8a2a2e6fe41
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-06-07 20:48:12 +0100

    merge

---
+++ 3rd/2nd/IOT/labs/src/Lab2/AWS_IoT/arduino_secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define SECRET_BROKER  ""
+#define SECRET_GROUP  ""
---
+++ 3rd/2nd/IOT/labs/src/Lab3/snoopy.py
@@ -0,0 +1,105 @@
+# /*
+# * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License").
+# * You may not use this file except in compliance with the License.
+# * A copy of the License is located at
+# *
+# *  http://aws.amazon.com/apache2.0
+# *
+# * or in the "license" file accompanying this file. This file is distributed
+# * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# * express or implied. See the License for the specific language governing
+# * permissions and limitations under the License.
+# */
+
+
+import os
+import sys
+import time
+import uuid
+import json
+import logging
+import argparse
+from AWSIoTPythonSDK.core.greengrass.discovery.providers import DiscoveryInfoProvider
+from AWSIoTPythonSDK.core.protocol.connection.cores import ProgressiveBackOffCore
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.exception.AWSIoTExceptions import DiscoveryInvalidRequestException
+
+AllowedActions = ['both', 'publish', 'subscribe']
+
+# General message notification callback
+def customOnMessage(message):
+    print('Received message on topic %s: %s\n' % (message.topic, message.payload))
+
+MAX_DISCOVERY_RETRIES = 10
+GROUP_CA_PATH = "./groupCA/"
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--ipaddress", action="store", required=True, dest="host", help="Local Greengrass IP address")
+parser.add_argument("-g", "--groupCA", action="store", dest="groupCA", required=True, help="Group CA of the greengrass core")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-n", "--thingName", action="store", dest="thingName", default="Bot", help="Targeted thing name")
+parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+parser.add_argument("-M", "--message", action="store", dest="message", default="Hello World!",
+                    help="Message to publish")
+
+args = parser.parse_args()
+host = args.host
+groupCA = args.groupCA
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+clientId = args.thingName
+thingName = args.thingName
+topic = args.topic
+
+if not args.certificatePath or not args.privateKeyPath:
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Iterate through all connection options for the core and use the first successful one
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureCredentials(groupCA, privateKeyPath, certificatePath)
+myAWSIoTMQTTClient.onMessage = customOnMessage
+
+connected = False
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+try:
+    myAWSIoTMQTTClient.connect()
+    connected = True
+except BaseException as e:
+    print("Error in connect!")
+    print("Type: %s" % str(type(e)))
+    print("Error message: %s" % e.message)
+
+if not connected:
+    print("Cannot connect to core %s. Exiting..." % coreInfo.coreThingArn)
+    sys.exit(-2)
+
+# Successfully connected to the core
+if args.mode == 'both' or args.mode == 'subscribe':
+    myAWSIoTMQTTClient.subscribe(topic, 0, None)
+time.sleep(2)
+
+loopCount = 0
+while True:
+    if args.mode == 'both' or args.mode == 'publish':
+        message = {}
+        message['message'] = args.message
+        message['sequence'] = loopCount
+        messageJson = json.dumps(message)
+        myAWSIoTMQTTClient.publish(topic, messageJson, 0)
+        if args.mode == 'publish':
+            print('Published topic %s: %s\n' % (topic, messageJson))
+        loopCount += 1
+    time.sleep(1)
